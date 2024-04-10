dbbe393af23c0ba5b429c8beb647f3251da3eddd
Neeraj Bhatt <neeraj1bh@gmail.com>
2024-04-10 18:30:24 +0530

    Add configuration files and initial code for complete React state

---
+++ 04-complete-react-state/src/app/components/CartContext.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React, { createContext, useContext, useState } from "react";
+import { type Cart } from "@/api/types";
+
+const useCartState = (initialCart: Cart) => useState<Cart>(initialCart);
+
+export const CartContext = createContext<ReturnType<
+  typeof useCartState
+> | null>(null);
+
+export const useCart = () => {
+  const cart = useContext(CartContext);
+  if (!cart) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+  return cart;
+};
+
+const CartProvider = ({
+  cart: initialCart,
+  children,
+}: {
+  cart: Cart;
+  children: React.ReactNode;
+}) => {
+  const [cart, setCart] = useCartState(initialCart);
+
+  return (
+    <CartContext.Provider value={[cart, setCart]}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
