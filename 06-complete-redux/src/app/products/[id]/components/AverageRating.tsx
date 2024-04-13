305ca95617d309ff95ecc9872630dca0b6ab819e
Neeraj Bhatt <neeraj1bh@gmail.com>
2024-04-13 13:24:28 +0530

    add redux state to average rating

--- 06-complete-redux/src/app/products/[id]/components/AverageRating.tsx
+++ 06-complete-redux/src/app/products/[id]/components/AverageRating.tsx
@@ -1,7 +1,22 @@
 "use client";
 import { Review } from "@/api/types";
+import { RootState, setReviews, useReviews } from "@/app/store/store";
+import { useRef } from "react";
+import { useStore } from "react-redux";
 
-export default function AverageRating({ reviews }: { reviews: Review[] }) {
+export default function AverageRating({
+  reviews: initialReviews,
+}: {
+  reviews: Review[];
+}) {
+  const store = useStore<RootState>();
+  const initialized = useRef(false);
+  if (!initialized.current) {
+    store.dispatch(setReviews(initialReviews));
+    initialized.current = true;
+  }
+
+  const reviews = useReviews();
   return (
     <>
       {reviews && reviews?.length && (
